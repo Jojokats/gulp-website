00b8b6feafba1b236db96853cfb1f0d1acd926ea
Joanna <morningstarjk@hotmail.com>
2017-08-17 13:16:08 -0400

    Updated gulpfile by adding new plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,36 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var livereload = require('gulp-livereload');
+var concat = require('gulp-concat');
+var minifyCss = require('gulp-minify-css');
+var autoprefixer = require('gulp-autoprefixer');
+var plumber = require('gulp-plumber');
+var sourcemaps = require('gulp-sourcemaps');
 
 // File paths
-var SCRIPTS_PATH = 'public/scripts/**/*.js'
+var DIST_PATH = 'public/dist';
+var SCRIPTS_PATH = 'public/scripts/**/*.js';
+var CSS_PATH = 'public/css/**/*.css';
 
 // Styles
 gulp.task('styles', function () {
 	console.log('starting styles task');
+
+	return gulp.src(['public/css/reset.css', CSS_PATH])
+		.pipe(plumber(function(err) {
+			console.log('Styles Task Error');
+			console.log(err);
+			this.emit('end');
+		}))
+		.pipe(autoprefixer())
+		//Here you are targeting the last 2 versions of all mahor browser and ie 8.
+		// .pipe(autoprefixer(
+		// 	browers: ['last 2 version', 'ie 8']
+		// ))
+		.pipe(concat('styles.css'))
+		.pipe(minifyCss())
+		.pipe(gulp.dest(DIST_PATH))
+		.pipe(livereload());
 });
 
 // Scripts
@@ -16,7 +39,7 @@ gulp.task('scripts', function () {
 
 	return gulp.src(SCRIPTS_PATH)
 		.pipe(uglify())
-		.pipe(gulp.dest('public/dist'))
+		.pipe(gulp.dest(DIST_PATH))
 		.pipe(livereload());
 });
 
@@ -33,7 +56,9 @@ gulp.task('default', function () {
 
 gulp.task('watch', function () {
 	console.log('Starting watch task');
+
 	require('./server.js');
 	livereload.listen();
 	gulp.watch(SCRIPTS_PATH, ['scripts']);
+	gulp.watch(CSS_PATH, ['styles']);
 });
